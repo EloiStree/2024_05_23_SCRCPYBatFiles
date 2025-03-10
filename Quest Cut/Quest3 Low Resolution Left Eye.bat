b4dda6479ef363287b367ee7b7a5cc0b492928f4
Eloi Stree <eloistree@gmail.com>
2025-03-10 18:47:31 +0100

    save

--- Quest Cut/Quest3 Low Resolution Left Eye.bat
+++ Quest Cut/Quest3 Low Resolution Left Eye.bat
@@ -1,4 +1,4 @@
 cd .. 
-scrcpy.exe --crop 2000:2000:0:0 --keyboard=uhid --video-source=display --audio-source=output --no-audio --orientation=0 --max-size 1024 --max-fps 20 
+scrcpy.exe --crop 1920:1920:0:0 --keyboard=uhid --video-source=display --audio-source=output --no-audio --orientation=0 --max-size 1024 --max-fps 20 
 
 timeout 10
